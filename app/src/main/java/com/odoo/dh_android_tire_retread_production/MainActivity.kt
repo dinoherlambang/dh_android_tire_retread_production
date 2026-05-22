567994f15eff48b6897c92837c57f314ce26d0b6
dinoherlambang <dino.herlambang@gmail.com>
2026-05-22 16:42:46 +0700

    revise the themes

--- app/src/main/java/com/odoo/dh_android_tire_retread_production/MainActivity.kt
+++ app/src/main/java/com/odoo/dh_android_tire_retread_production/MainActivity.kt
@@ -2,6 +2,7 @@ package com.odoo.dh_android_tire_retread_production
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.odoo.dh_android_tire_retread_production.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
